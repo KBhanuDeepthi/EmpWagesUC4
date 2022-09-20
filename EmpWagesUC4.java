866bc2fcb35f0f750ed6fb857b2a5d995db19931
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-20 22:00:00 +0530

    Added: EmpWage_Usecase4 logic

---
+++ EmpWagesUC4.java
@@ -0,0 +1,26 @@
+class EmpWagesUC4{
+
+	public static void main(String[] args) {
+		final int IS_PART_TIME=1;
+		final int IS_FULL_TIME=2;
+		final int EMP_RATE_PER_HOUR=20;
+		//variables
+		int empHrs=0;
+		int empWage=0;
+		int empcheck=(int)Math.floor(Math.random()*10)%3;
+		switch(empcheck) {
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		default:
+		    empHrs=0;
+		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("EmpWage:"+empWage);
+
+	}
+
+}
